3c65612a1d3eed30fc6dce8ec7b7b30444eb5dfa
Dayco72 <139602627+Dayco72@users.noreply.github.com>
2025-05-04 15:17:29 -0600

    Add files via upload

---
+++ PC.v
@@ -0,0 +1,10 @@
+module PC(
+    input [31:0] IN,
+    input CLK,
+    output reg [31:0] OUT = 32'd0   // Inicializado a 0
+);
+
+always @(posedge CLK) begin
+    OUT = IN;
+end	
+endmodule
---
+++ UnidadDeControl.v
@@ -0,0 +1,35 @@
+module UnidadDeControl (
+    input  wire [5:0]  OpCode,
+    output reg         RegDst,
+	output reg         Branch,
+    output reg         MemRead,
+	output reg         MemToReg,
+	output reg [2:0]   ALUOp,
+    output reg         MemToWrite,
+	output reg         ALUSrc,
+	output reg         RegWrite
+    
+);
+    always @(*) begin
+        // Valores por defecto
+        MemToReg  = 1'b0;
+        MemToWrite  = 1'b0;
+        ALUOp     = 3'b000;
+        RegWrite  = 1'b0;
+		RegDst = 1'b0;
+		Branch = 1'b0;
+		MemRead = 1'b0;
+		ALUSrc = 1'b0;
+		
+        case (OpCode)
+            6'b000000: begin // Instuccion R
+                RegWrite = 1'b1;
+                ALUOp    = 3'b010;
+				RegDst = 1'b1;
+            end
+            default: begin
+                // Aquí se pueden agregar otros opcodes
+            end
+        endcase
+    end
+endmodule
